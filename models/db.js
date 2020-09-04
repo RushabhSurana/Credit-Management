c0731be41c2549038eade3fb396cce6165c3e281
Rushabh Surana <rushabhsurana147@gmail.com>
2020-09-05 00:05:56 +0530

    Passkey

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://rushabh:12345@cluster0.pvsqj.mongodb.net/UsersDB?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true  },(err) =>{
+mongoose.connect('mongodb+srv://rushabh:password@cluster0.pvsqj.mongodb.net/UsersDB?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true  },(err) =>{
     if(!err){ console.log('MongoDB Connection Succeeded.')}
     else {console.log('Error in DB connection: '+err)}
 });
